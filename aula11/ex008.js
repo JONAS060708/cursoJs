06aaddd8141aab464d5c83e22f06838b39bc74c0
Jonas Cabral <cabral_jonascabral@hotmail.com>
2021-01-15 17:30:05 -0300

    Condições javaScript funções

---
+++ aula11/ex008.js
@@ -0,0 +1,10 @@
+var vel = 60
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel > 60) {
+    console.log(`Velocidade acima do permitido, você foi multado!`)
+    
+}else{
+    console.log(`Boa viajem!`)
+}
+
