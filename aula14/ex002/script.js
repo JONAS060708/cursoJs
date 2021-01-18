3a34923e5d5700893ec7f3372cb47a349a991a08
Jonas Cabral <cabral_jonascabral@hotmail.com>
2021-01-18 17:52:49 -0300

    Exercicio tabuada lista de seleção

--- aula14/ex002/script.js
+++ aula14/ex002/script.js
@@ -1,1 +1,25 @@
-alert('ola')
+//alert('ola')
+
+function gerar() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    
+    if (num.value.length < 1 ) {
+        alert('Digite um numero!')        
+    } else{
+
+        let n = Number(num.value)
+        let cont = 0
+        tab.innerHTML=''
+        while (cont <= 10) {
+            let items = document.createElement('option')           
+            
+            items.text = `${n} X ${cont} = ${ n * cont}`
+            items.value = `${cont}`
+            tab.appendChild(items)
+            
+            cont++
+        }
+    }
+
+}
